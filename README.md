b40c3e3f425fc29824e5da2ed4ae6c4357c2be98
Karlis Vesters <karlis.vesters@gmail.com>
2020-11-18 01:45:07 +0200

    .NET5 featured version (VSCode vs Rider) & @boatik review

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# Extract Jenkins jobs .NET5
+
+This script allows to extract Jenkins job names from Jenkins API.  
+To generate Jenkins API token (password) for user please use this [link](https://www.jenkins.io/blog/2018/07/02/new-api-token-system/).
+
+Program requires two parameters user and API token to access Jenkins server.  
+Execution example:
+
+```batch
+jenkins-extract-jobs.exe admin "11c4d97ce9b3dbc4d675d6fab32ebd9a4a" "http://localhost:8080/"
+```
+
+## Important notice
+
+This script does not include any error processing. This programm should be used only as an example how to get data from Jenkins API.  
+Any improvement are welcome.
+
+## Docker setup for testing
+
+```bash
+docker run -d -p 8080:8080 jenkins/jenkins
+```
+
+Create user admin/admin.  
+Create token "http://localhost:8080/user/admin/configure"
---
+++ jenkins-extract-jobs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace jenkins_extract_jobs
+{
+    static class Program
+    {
+        private static List<StructJenkinsJobs> _jenkinsJobs = new List<StructJenkinsJobs>();
+
+        private static void AddJobToList(StructJenkinsJobs job)
+        {
+            _jenkinsJobs.Add(job);
+        }
+
+        private static async Task Main(string[] args)
+        {
+            var username = args[0];
+            var password = args[1];
+            var urlJenkins = args[2];
+            var folder = "root"; //jobs which are in root listing
+
+            var auth = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(
+                    System.Text.Encoding.ASCII.GetBytes(
+                        $"{username}:{password}")));
+
+            await AddFolderJobs(folder, urlJenkins, auth);
+
+            foreach (var jobEntity in _jenkinsJobs)
+            {
+                Console.WriteLine($"{jobEntity.Url} / {jobEntity.Folder} / {jobEntity.Name}");
+            }
+        }
+
+        private record StructJenkinsJobs
+        {
+            public string Folder { get; set; }
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+
+        private static async Task AddFolderJobs(string folder, string url, AuthenticationHeaderValue auth)
+        {
+            url = $"{url}api/json";
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = auth;
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            using var jenkinsJson =
+                await JsonDocument.ParseAsync(await httpClient.GetStreamAsync(url));
+
+            JsonElement jsonRoot = jenkinsJson.RootElement;
+
+            foreach (var job in jsonRoot.GetProperty("jobs").EnumerateArray())
+            {
+                if (job.GetProperty("_class").ToString().Equals("com.cloudbees.hudson.plugins.folder.Folder",
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    await AddFolderJobs(job.GetProperty("name").ToString(), job.GetProperty("url").ToString(), auth);
+                    continue;
+                }
+
+                var jobEntity = new StructJenkinsJobs
+                {
+                    Folder = folder, Name = job.GetProperty("name").ToString(),
+                    Url = job.GetProperty("url").ToString()
+                };
+                AddJobToList(jobEntity);
+            }
+        }
+    }
+}
